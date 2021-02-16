3e9509e33f9479aa4eceaaa62d5c549cb561f656
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-02-15 21:53:59 -0300

    add board and player skeletons

---
+++ board.js
@@ -0,0 +1,70 @@
+class Board {
+    constructor(columns, rows, partsWidth, partsHeight) {
+        this.reference = document.querySelector('canvas');
+        this.context = this.reference.getContext('2d');
+        this.totalWidth = rows * partsWidth;
+        this.totalHeight = columns * partsHeight;
+        this.columns = columns;
+        this.rows = rows;
+        this.partsWidth = partsWidth;
+        this.partsHeight = partsHeight;
+        this.map = [];
+    }
+
+    
+    build() {
+        for (let i = 0; i < this.rows; i++) {
+            this.map.push([]);
+            
+            for(let j = 0; j < this.columns; j++) {
+                this.map[i].push({x: j * this.partsWidth, y: i * this.partsHeight, coordinates: {x: j, y: i}})
+            }
+        } 
+    }
+    
+    drawMap() {
+        this.reference.setAttribute('width', `${this.totalWidth}`);
+        this.reference.setAttribute('height', `${this.totalHeight}`);
+    
+        for (let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.columns; j++) {
+                this.context.beginPath()
+                this.context.strokeRect(j * this.partsWidth, i * this.partsHeight, this.partsWidth, this.partsHeight)
+                this.context.closePath()
+            }
+        } 
+    }
+
+    drawPlayer(player, width, height) {
+        const x = this.map[player.x][player.y].x;
+        const y = this.map[player.x][player.y].y;
+        const centeringFactor = (this.partsWidth - width )/ 2;
+        // console.log(calc)
+        this.context.fillStyle = 'red'
+        this.context.beginPath()
+        this.context.fillRect(x + centeringFactor, y + centeringFactor, width, height)
+        this.context.closePath()
+    }
+}
+
+class Player {
+    constructor(x, y, health, strenght) {
+        this.x = x;
+        this.y = y;
+        this.health = health;
+        this.strenght = strenght;
+    }
+}
+
+let dungeon = new Board(5, 5, 100, 100)
+dungeon.build()
+dungeon.drawMap()
+
+let player = new Player(0, 0, 20, 7)
+
+dungeon.drawPlayer(player, 50, 50)
+
+console.log(player)
+// console.log(dungeon.map[4][0].x);
+
+// console.log(document)
