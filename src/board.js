1f9ba6307f0841e2ce064629356a36df69855403
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-02-16 22:41:24 -0300

    add dice elements and function, click event for canvas parts, and basic movement logic

--- src/board.js
+++ src/board.js
@@ -1,13 +1,13 @@
 class Board {
-    constructor(columns, rows, partsWidth, partsHeight) {
+    constructor(columns, rows, parcialWidth, parcialHeight) {
         this.reference = document.querySelector('canvas');
         this.context = this.reference.getContext('2d');
-        this.totalWidth = rows * partsWidth;
-        this.totalHeight = columns * partsHeight;
+        this.totalWidth = rows * parcialWidth;
+        this.totalHeight = columns * parcialHeight;
         this.columns = columns;
         this.rows = rows;
-        this.partsWidth = partsWidth;
-        this.partsHeight = partsHeight;
+        this.parcialWidth = parcialWidth;
+        this.parcialHeight = parcialHeight;
         this.map = [];
     }
 
@@ -17,7 +17,7 @@ class Board {
             this.map.push([]);
             
             for(let j = 0; j < this.columns; j++) {
-                this.map[i].push({x: j * this.partsWidth, y: i * this.partsHeight, coordinates: {x: j, y: i}})
+                this.map[i].push({x: j * this.parcialWidth, y: i * this.parcialHeight, coordinates: {x: j, y: i}})
             }
         } 
     }
@@ -29,37 +29,54 @@ class Board {
         for (let i = 0; i < this.rows; i++) {
             for(let j = 0; j < this.columns; j++) {
                 this.context.beginPath()
-                this.context.strokeRect(j * this.partsWidth, i * this.partsHeight, this.partsWidth, this.partsHeight)
+                this.context.strokeRect(j * this.parcialWidth, i * this.parcialHeight, this.parcialWidth, this.parcialHeight)
                 this.context.closePath()
             }
         } 
     }
 
-    drawPlayer(player, width, height) {
-        const x = this.map[player.x][player.y].x;
-        const y = this.map[player.x][player.y].y;
-        const centeringFactor = (this.partsWidth - width )/ 2;
+    drawComponent(component) {
+        const x = this.map[component.y][component.x].x;
+        const y = this.map[component.y][component.x].y;
+        const xCenteringFactor = (this.parcialWidth - component.width ) / 2;
+        const yCenteringFactor = (this.parcialHeight - component.height) / 2
+
         // console.log(calc)
         this.context.fillStyle = 'red'
         this.context.beginPath()
-        this.context.fillRect(x + centeringFactor, y + centeringFactor, width, height)
+        this.context.fillRect(x + xCenteringFactor, y + yCenteringFactor, component.width, component.height)
         this.context.closePath()
+    }
 
-        // let i = 0
-        // for (let i = 0; i < 2; i++) {
-            // setInterval(() => {
-            //     i++
-            //     this.context.clearRect(x + centeringFactor, y + centeringFactor, width, height)
-            //     this.context.beginPath()
-            //     this.context.fillRect(x + (i * 100) + centeringFactor, y + (i * 100) + centeringFactor, width, height)
-            //     this.context.closePath()
-            // }, 1000)
-        // }
+    rollDice() {
+        const result = Math.floor(Math.random() * 6 + 1);
+        document.getElementById('dice-result').innerHTML = result;
+
+        return result;
+    }
+
+    move (component, x, y, movements) {
+        // let 
+        // if()
+
+        component.x = x;
+        component.y = y;
+        
+
+        this.drawAll(component)
+    }
+
+    drawAll(component) {
+        this.context.clearRect(0, 0, this.totalWidth, this.totalHeight);
+        this.drawMap();
+        this.drawComponent(component);
     }
 }
 
 class Player {
     constructor(x, y, health, strenght) {
+        this.width = 50;
+        this.height = 50;
         this.x = x;
         this.y = y;
         this.health = health;
@@ -71,17 +88,32 @@ let dungeon = new Board(5, 5, 100, 100)
 dungeon.build()
 dungeon.drawMap()
 
-let player = new Player(0, 0, 20, 7)
+let player = new Player(4, 3, 20, 7)
 
-dungeon.drawPlayer(player, 50, 50)
+dungeon.drawComponent(player)
 
-console.log(player)
-// console.log(dungeon.map[4][0].x);
 
-// console.log(document)
+let diceButton = document.querySelector('.dice button')
+diceButton.addEventListener('click', () => {
+    const movements = dungeon.rollDice();
 
-console.log(dungeon.reference.offsetTop)
+    dungeon.reference.addEventListener('click', event => {
+        let leftCoordinate = dungeon.reference.offsetLeft + dungeon.reference.clientLeft;
+        let topCoordinate = dungeon.reference.offsetTop + dungeon.reference.clientTop;
+    
+        let clickedElementX = Math.floor((event.pageX - leftCoordinate) / dungeon.parcialWidth);
+        let clickedElementY = Math.floor((event.pageY - topCoordinate) / dungeon.parcialHeight);
 
-dungeon.reference.addEventListener('click', event => {
-    console.log(event.pageX, event.pageY)
+        
+    
+        dungeon.move(player, clickedElementX, clickedElementY, movements)
+    
+        // console.log(dungeon.map)
+        // console.log(dungeon.rollDice());
+        
+        // console.log(event.pageX, event.pageY)
+    })
 })
+
+
+
